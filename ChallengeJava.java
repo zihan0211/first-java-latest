2020f1aaeba9f670a671203ef47d1f2c82598f29
zihan0211 <zihanfahira13@gmail.coom>
2021-10-12 15:48:36 +0700

    Initial Commit

---
+++ ChallengeJava.java
@@ -0,0 +1,21 @@
+public class ChallengeJava {
+    public static void main (String[] args) {
+       String firsName = "Zihan ";
+       String lastName = "Fahira ";
+       String address = "Jalan_Agung";
+       String hoby = "Membaca_Novel ";
+       String favoriteFood = "Steak ";
+       String favoriteDrink = "Orange_Juice ";
+       int age = 20;
+       String numberhp = "0825733837 ";
+       System.out.println("First Name :" + firsName);
+       System.out.println("Last Name :" + lastName);
+       System.out.println("Address :" + address );
+       System.out.println("Hobby :" + hoby);
+       System.out.println("Favorite Food :" + favoriteFood);
+       System.out.println("Favorite Drink :" + favoriteDrink);
+       System.out.println("Age :" + age);
+       System.out.println("Phone Number :" + numberhp);
+
+    }
+}
